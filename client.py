464c47bdc3429a1e04493c4156c7b0b0be9f95a9
loong <i@zhanwei.me>
2018-12-31 14:44:10 +0800

    实现了文件传输，优化代码规范

--- client.py
+++ client.py
@@ -5,7 +5,8 @@ import threading
 import sys
 import html
 import pickle
-from PyQt5.QtWidgets import QApplication, QMainWindow, QHeaderView, QAbstractItemView, QTableWidgetItem, QMessageBox
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QHeaderView, QAbstractItemView, QTableWidgetItem, QMessageBox, QFileDialog
 from PyQt5.QtCore import pyqtSlot, QThread, pyqtSignal, Qt
 from PyQt5.QtGui import QIcon
 from client_ui import *
@@ -22,70 +23,104 @@ class SendThread(QThread):
     update_label_signal = pyqtSignal(int, int)
     update_log_signal = pyqtSignal(int, str)
 
-    def __init__(self, data, parent=None):
+    def __init__(self, data, file_name, parent=None):
         super(SendThread, self).__init__(parent)
         self.data = data
+        self.fileName = file_name
         self.sendPackets = 0
         self.lossPackets = 0
         self.continuousLoss = 0
         print("sendThread")
 
-    def buildPacket(self, data):
+    def build_message_packet(self, data):
         size = len(data)
-        self.update_log_signal.emit(0, "开始数据传送，总大小 " + str(size) + " Bytes")
-        self.sendPacket(0, [str(0), str(size)])
+        self.update_log_signal.emit(0, "开始数据传送，数据类型【消息】，总大小 " + str(size) + " Bytes")
+        self.send_packet(0, [str(0), str(size), self.fileName])
         send_id = 1
-        packData = [str(send_id)]
-        packSize = len(packData)
+        pack_data = [str(send_id)]
+        pack_size = len(pack_data)
         buffer = bytearray(b'')
         for i in range(0, size):
-            packSize = packSize + len(data[i:i + 1])
+            pack_size = pack_size + len(data[i:i + 1])
             buffer = buffer + data[i:i + 1]
-            if (packSize + 47 >= MAX_PACK_SIZE or i == size - 1):
-                packData.append(buffer)
-                print(packData)
+            if pack_size + 47 >= MAX_PACK_SIZE or i == size - 1:
+                pack_data.append(buffer)
+                print(pack_data)
                 print("进入发包流程 编号" + str(send_id))
-                self.sendPacket(send_id, packData)
-                if(self.continuousLoss >= 10):
+                self.send_packet(send_id, pack_data)
+                if self.continuousLoss >= 10:
                     self.update_log_signal.emit(2, "发送失败！网络通讯出现异常，请检查服务端是否在线")
                     break
                 send_id = send_id + 1
                 print("包编号 " + str(send_id) + " 已确认到达")
-                packData = [str(send_id)]
-                packSize = len(packData)
+                pack_data = [str(send_id)]
+                pack_size = len(pack_data)
                 buffer = bytearray(b'')
 
-    def sendPacket(self, send_id, packData):
+    def build_file_packet(self, file):
+        size = os.path.getsize(file)
+        self.update_log_signal.emit(0, "开始数据传送，数据类型【文件】，总大小 " + str(size) + " Bytes")
+        self.send_packet(0, [str(0), str(size), self.fileName])
+        send_id = 1
+        pack_data = [str(send_id)]
+        pack_size = len(pack_data)
+        buffer = bytearray(b'')
+        with open(file, 'rb') as f:
+            for i in range(0, size):
+                pack_size = pack_size + 1
+                buffer = buffer + f.read(1)
+                if pack_size + 47 >= MAX_PACK_SIZE or i == size - 1:
+                    pack_data.append(buffer)
+                    print(pack_data)
+                    print("进入发包流程 编号" + str(send_id))
+                    self.send_packet(send_id, pack_data)
+                    if self.continuousLoss >= 10:
+                        self.update_log_signal.emit(2, "发送失败！网络通讯出现异常，请检查服务端是否在线")
+                        break
+                    send_id = send_id + 1
+                    print("包编号 " + str(send_id) + " 已确认到达")
+                    pack_data = [str(send_id)]
+                    pack_size = len(pack_data)
+                    buffer = bytearray(b'')
+        f.close()
+
+    def send_packet(self, send_id, pack_data):
         global resend
         resend = True
-        while (resend and self.continuousLoss < 10):
+        while resend and self.continuousLoss < 10:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            subthread = threading.Thread(target=self.sendProcess, args=(send_id, pickle.dumps(packData), s))
+            subthread = threading.Thread(target=self.send_process, args=(send_id, pickle.dumps(pack_data), s))
             subthread.start()
             time.sleep(0.2)
             s.close()
 
-    def sendProcess(self, send_id, data, s):
+    def send_process(self, send_id, data, s):
         global resend
         self.sendPackets = self.sendPackets + 1
-        packetSize = s.sendto(data, (DEST_IP, DEST_PORT))
+        packet_size = s.sendto(data, (DEST_IP, DEST_PORT))
         try:
             (rawData, addr) = s.recvfrom(1024)
-            if (int(rawData.decode()) == send_id):
+            if int(rawData.decode()) == send_id:
                 self.continuousLoss = 0
                 resend = False
-                self.update_table_signal.emit(str(send_id), str(packetSize), '确认到达')
+                self.update_table_signal.emit(str(send_id), str(packet_size), '确认到达')
                 self.update_log_signal.emit(1, "包编号 " + str(send_id) + " 确认到达")
-        except Exception:
+        except Exception as e:
             self.lossPackets = self.lossPackets + 1
             self.continuousLoss = self.continuousLoss + 1
-            self.update_table_signal.emit(str(send_id), str(packetSize), '丢失，等待重传')
+            self.update_table_signal.emit(str(send_id), str(packet_size), '丢失，等待重传')
             self.update_log_signal.emit(2, "包编号 " + str(send_id) + " 丢失，正在重传...")
             print("包丢失，即将重传...")
+            print(e)
 
     def run(self):
         print("开工！")
-        self.buildPacket(self.data)
+        if self.fileName:
+            print("文件传送开始")
+            self.build_file_packet(self.data)
+        else:
+            print("消息传送开始")
+            self.build_message_packet(self.data)
         self.update_label_signal.emit(self.sendPackets, self.lossPackets)
         self.update_log_signal.emit(0, "数据传送完毕！")
         self.send_over_signal.emit()
@@ -106,19 +141,20 @@ class ClientWindow(QMainWindow, Ui_udpClient):
         self.recentTable.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.allSendPackets = 0
         self.allLossPackets = 0
+        self.selectedFile = ""
         self.send.clicked.connect(self.on_send_click)
         self.apply.clicked.connect(self.on_apply_click)
+        self.chooseFile.clicked.connect(self.openfile)
         self.print_log(0, "软件初始化完毕，欢迎使用！")
 
     @pyqtSlot()
     def on_send_click(self):
         data = self.input.toPlainText()
-        if(data):
-            self.input.clear()
+        if data:
             self.recentTable.setRowCount(0)
             self.send.setEnabled(False)
             self.send.setText("发送中")
-            self.sendThread = SendThread(data.encode())
+            self.sendThread = SendThread(data.encode(), "")
             self.sendThread.send_over_signal.connect(self.on_send_over)
             self.sendThread.update_table_signal.connect(self.update_table)
             self.sendThread.update_label_signal.connect(self.update_label)
@@ -126,6 +162,24 @@ class ClientWindow(QMainWindow, Ui_udpClient):
             self.sendThread.start()
             self.browser.append("<font color=\"#1E90FF\" size=\"2\">" + html.escape("------->>> " + DEST_IP + ":" + str(DEST_PORT)) + "</font>")
             self.browser.append(data)
+            self.input.clear()
+            self.file.setText("未选择文件...")
+            self.selectedFile = ""
+        elif self.selectedFile:
+            self.recentTable.setRowCount(0)
+            self.send.setEnabled(False)
+            self.send.setText("发送中")
+            self.sendThread = SendThread(self.selectedFile, self.selectedFile.split('/')[-1])
+            self.sendThread.send_over_signal.connect(self.on_send_over)
+            self.sendThread.update_table_signal.connect(self.update_table)
+            self.sendThread.update_label_signal.connect(self.update_label)
+            self.sendThread.update_log_signal.connect(self.print_log)
+            self.sendThread.start()
+            self.browser.append("<font color=\"#1E90FF\" size=\"2\">" + html.escape("------->>> " + DEST_IP + ":" + str(DEST_PORT)) + "</font>")
+            self.browser.append("正在发送文件：" + self.selectedFile)
+            self.input.clear()
+            self.file.setText("未选择文件...")
+            self.selectedFile = ""
 
     @pyqtSlot()
     def on_send_over(self):
@@ -133,26 +187,34 @@ class ClientWindow(QMainWindow, Ui_udpClient):
         self.send.setText("发送")
         self.send.setEnabled(True)
 
+    @pyqtSlot()
+    def openfile(self):
+        file_name = QFileDialog.getOpenFileName(self, '选择文件', '', '')
+        print(file_name)
+        if file_name:
+            self.selectedFile = file_name[0]
+            self.file.setText(file_name[0].split('/')[-1])
+
     @pyqtSlot(str, str, str)
-    def update_table(self, packetID, size, message):
-        rowCount = self.recentTable.rowCount()
-        self.recentTable.setRowCount(rowCount + 1)
-        print(rowCount)
-        print(packetID)
+    def update_table(self, packet_id, size, message):
+        row_count = self.recentTable.rowCount()
+        self.recentTable.setRowCount(row_count + 1)
+        print(row_count)
+        print(packet_id)
         print(size)
         print(message)
-        self.recentTable.setItem(rowCount, 0, QTableWidgetItem(packetID))
-        self.recentTable.setItem(rowCount, 1, QTableWidgetItem(size))
-        self.recentTable.setItem(rowCount, 2, QTableWidgetItem(message))
+        self.recentTable.setItem(row_count, 0, QTableWidgetItem(packet_id))
+        self.recentTable.setItem(row_count, 1, QTableWidgetItem(size))
+        self.recentTable.setItem(row_count, 2, QTableWidgetItem(message))
         self.recentTable.scrollToBottom()
         for index in range(self.recentTable.columnCount()):
-            self.recentTable.item(rowCount, index).setTextAlignment(Qt.AlignCenter)
+            self.recentTable.item(row_count, index).setTextAlignment(Qt.AlignCenter)
 
     @pyqtSlot(int, int)
-    def update_label(self, sendPackets, lossPackets):
-        self.recentAnal.setText("本次传送 " + str(sendPackets) + " 个包，重传 " + str(lossPackets) + " 个包")
-        self.allSendPackets = self.allSendPackets + sendPackets
-        self.allLossPackets = self.allLossPackets + lossPackets
+    def update_label(self, send_packets, loss_packets):
+        self.recentAnal.setText("本次传送 " + str(send_packets) + " 个包，重传 " + str(loss_packets) + " 个包")
+        self.allSendPackets = self.allSendPackets + send_packets
+        self.allLossPackets = self.allLossPackets + loss_packets
         self.allAnal.setText("累计传送 " + str(self.allSendPackets) + " 个包，重传 " + str(self.allLossPackets) + " 个包")
 
     @pyqtSlot()
@@ -165,15 +227,15 @@ class ClientWindow(QMainWindow, Ui_udpClient):
 
     @pyqtSlot(int, str)
     def print_log(self, level, message):
-        nowTime = "<font color=\"#000000\" size=\"3\">" + html.escape("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "] ")
-        if(level == 0):
-            self.log.append(nowTime + "<font color=\"#1E90FF\" size=\"3\">" + html.escape(message) + "</font>")
-        elif (level == 1):
-            self.log.append(nowTime + "<font color=\"#66CD00\" size=\"3\">" + html.escape(message) + "</font>")
-        elif (level == 2):
-            self.log.append(nowTime + "<font color=\"#FF2D2D\" size=\"3\">" + html.escape(message) + "</font>")
-        elif (level == 3):
-            self.log.append(nowTime + "<font color=\"#000000\" size=\"3\">" + html.escape(message) + "</font>")
+        now_time = "<font color=\"#000000\" size=\"3\">" + html.escape("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "] ")
+        if level == 0:
+            self.log.append(now_time + "<font color=\"#1E90FF\" size=\"3\">" + html.escape(message) + "</font>")
+        elif level == 1:
+            self.log.append(now_time + "<font color=\"#66CD00\" size=\"3\">" + html.escape(message) + "</font>")
+        elif level == 2:
+            self.log.append(now_time + "<font color=\"#FF2D2D\" size=\"3\">" + html.escape(message) + "</font>")
+        elif level == 3:
+            self.log.append(now_time + "<font color=\"#000000\" size=\"3\">" + html.escape(message) + "</font>")
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 # code:utf8
 import socket
 import time
-import threading
+import os
 import sys
 import html
 import random
 import pickle
-from PyQt5.QtWidgets import QApplication, QMainWindow, QHeaderView, QAbstractItemView, QTableWidgetItem, QMessageBox
-from PyQt5.QtCore import pyqtSlot, QThread, pyqtSignal, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtCore import pyqtSlot, QThread, pyqtSignal
 from PyQt5.QtGui import QIcon
 from server_ui import *
 
@@ -17,6 +17,7 @@ DEST_PORT = 6666
 run = False
 dropRate = 0.0
 
+
 class ReceiveThread(QThread):
     update_message_signal = pyqtSignal(str)
     update_log_signal = pyqtSignal(int, str)
@@ -25,7 +26,7 @@ class ReceiveThread(QThread):
         super(ReceiveThread, self).__init__(parent)
         self.skt = skt
 
-    def receivePacket(self):
+    def receive_packet(self):
         try:
             skt = self.skt
             (rawData, addr) = skt.recvfrom(1024)
@@ -33,33 +34,64 @@ class ReceiveThread(QThread):
             skt.sendto(data[0].encode(), addr)
             print(addr)
             send_id = int(data[0])
-            if(send_id == 0):
-                size = int(data[1])
-                self.update_log_signal.emit(0, "开始数据接收，总大小 " + str(size) + " Bytes")
-                self.update_message_signal.emit("<font color=\"#1E90FF\" size=\"2\">" + html.escape("-------<<< " + str(addr[0]) + ":" + str(addr[1])) + "</font>")
-                self.receiveMessage(size, skt)
+            if send_id == 0:
+                if data[2]:
+                    size = int(data[1])
+                    file_name = data[2]
+                    self.update_log_signal.emit(0, "开始数据接收，数据类型【文件】，总大小 " + str(size) + " Bytes")
+                    self.update_message_signal.emit("<font color=\"#1E90FF\" size=\"2\">" + html.escape("-------<<< " + str(addr[0]) + ":" + str(addr[1])) + "</font>")
+                    self.receive_file(size, file_name, skt)
+                else:
+                    size = int(data[1])
+                    self.update_log_signal.emit(0, "开始数据接收，数据类型【消息】，总大小 " + str(size) + " Bytes")
+                    self.update_message_signal.emit("<font color=\"#1E90FF\" size=\"2\">" + html.escape("-------<<< " + str(addr[0]) + ":" + str(addr[1])) + "</font>")
+                    self.receive_message(size, skt)
         except Exception as e:
             print(e)
             self.update_log_signal.emit(2, "服务已停止！")
 
-    def receiveMessage(self, size, skt):
+    def receive_message(self, size, skt):
         try:
             received_id = 0
-            receivedData = b''
-            while (len(receivedData) < size):
+            received_data = b''
+            while len(received_data) < size:
                 (rawData, addr) = skt.recvfrom(1024)
                 data = pickle.loads(rawData)
-                if(random.uniform(0, 100) > dropRate):
+                if random.uniform(0, 100) > dropRate:
                     skt.sendto(data[0].encode(), addr)
                 send_id = int(data[0])
                 self.update_log_signal.emit(1, "包编号 " + str(send_id) + " 收到,已发送确认包")
-                if (send_id == received_id + 1):
+                if send_id == received_id + 1:
                     received_id = received_id + 1
-                    receivedData = receivedData + data[1]
+                    received_data = received_data + data[1]
             self.update_log_signal.emit(0, "数据接收完毕！")
-            self.update_message_signal.emit(receivedData.decode())
+            self.update_message_signal.emit(received_data.decode())
             self.update_message_signal.emit("<font color=\"#66CD00\" size=\"2\">" + "--------------------" + "</font>")
-            print(receivedData.decode())
+            print(received_data.decode())
+        except Exception as e:
+            print(e)
+            self.update_log_signal.emit(2, "服务已中断！")
+
+    def receive_file(self, size, file_name, skt):
+        try:
+            received_id = 0
+            received_size = 0
+            self.update_message_signal.emit("开始接收文件：" + file_name)
+            with open(file_name, 'wb') as f:
+                while received_size < size:
+                    (rawData, addr) = skt.recvfrom(1024)
+                    data = pickle.loads(rawData)
+                    if random.uniform(0, 100) > dropRate:
+                        skt.sendto(data[0].encode(), addr)
+                    send_id = int(data[0])
+                    self.update_log_signal.emit(1, "包编号 " + str(send_id) + " 收到,已发送确认包")
+                    if send_id == received_id + 1:
+                        received_id = received_id + 1
+                        f.write(data[1])
+                        received_size = received_size + len(data[1])
+                self.update_log_signal.emit(0, "数据接收完毕！")
+                self.update_message_signal.emit("文件接收完毕！保存至：" + os.getcwd())
+                self.update_message_signal.emit("<font color=\"#66CD00\" size=\"2\">" + "--------------------" + "</font>")
         except Exception as e:
             print(e)
             self.update_log_signal.emit(2, "服务已中断！")
@@ -67,7 +99,7 @@ class ReceiveThread(QThread):
     def run(self):
         while(run):
             print("开工！")
-            self.receivePacket()
+            self.receive_packet()
 
 
 class ClientWindow(QMainWindow, Ui_udpServer):
@@ -77,15 +109,15 @@ class ClientWindow(QMainWindow, Ui_udpServer):
         self.setWindowIcon(QIcon('./udp.png'))
         self.serverIP.setText(DEST_IP)
         self.serverPort.setText(str(DEST_PORT))
-        self.optButton.clicked.connect(self.serverControl)
-        self.modButton.clicked.connect(self.serverMod)
-        self.dropNow.clicked.connect(self.dropMod)
+        self.optButton.clicked.connect(self.server_control)
+        self.modButton.clicked.connect(self.server_mod)
+        self.dropNow.clicked.connect(self.drop_mod)
         self.print_log(0, "软件初始化完毕，欢迎使用！")
 
     @pyqtSlot()
-    def serverControl(self):
+    def server_control(self):
         global run
-        if(not run):
+        if not run:
             run = True
             self.skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.skt.bind((DEST_IP, DEST_PORT))
@@ -101,16 +133,16 @@ class ClientWindow(QMainWindow, Ui_udpServer):
             self.optButton.setText("启动服务")
 
     @pyqtSlot()
-    def serverMod(self):
+    def server_mod(self):
         global DEST_IP, DEST_PORT
         DEST_IP = self.serverIP.text()
         DEST_PORT = int(self.serverPort.text())
-        if(run):
-            self.serverControl()
+        if run:
+            self.server_control()
         QMessageBox.information(self, "设置更新", "服务设置已更新！\t\n\n服务IP: " + DEST_IP + "\t\n服务端口: " + str(DEST_PORT) + "\n请重新启动服务！", QMessageBox.Yes)
 
     @pyqtSlot()
-    def dropMod(self):
+    def drop_mod(self):
         try:
             global dropRate
             dropRate = float(self.dropRate.text())
@@ -124,15 +156,15 @@ class ClientWindow(QMainWindow, Ui_udpServer):
 
     @pyqtSlot(int, str)
     def print_log(self, level, message):
-        nowTime = "<font color=\"#000000\" size=\"3\">" + html.escape("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "] ")
-        if (level == 0):
-            self.log.append(nowTime + "<font color=\"#1E90FF\" size=\"3\">" + html.escape(message) + "</font>")
-        elif (level == 1):
-            self.log.append(nowTime + "<font color=\"#66CD00\" size=\"3\">" + html.escape(message) + "</font>")
-        elif (level == 2):
-            self.log.append(nowTime + "<font color=\"#FF2D2D\" size=\"3\">" + html.escape(message) + "</font>")
-        elif (level == 3):
-            self.log.append(nowTime + "<font color=\"#000000\" size=\"3\">" + html.escape(message) + "</font>")
+        now_time = "<font color=\"#000000\" size=\"3\">" + html.escape("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "] ")
+        if level == 0:
+            self.log.append(now_time + "<font color=\"#1E90FF\" size=\"3\">" + html.escape(message) + "</font>")
+        elif level == 1:
+            self.log.append(now_time + "<font color=\"#66CD00\" size=\"3\">" + html.escape(message) + "</font>")
+        elif level == 2:
+            self.log.append(now_time + "<font color=\"#FF2D2D\" size=\"3\">" + html.escape(message) + "</font>")
+        elif level == 3:
+            self.log.append(now_time + "<font color=\"#000000\" size=\"3\">" + html.escape(message) + "</font>")
 
 
 if __name__ == "__main__":
